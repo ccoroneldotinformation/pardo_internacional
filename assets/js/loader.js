d1b5180281414fc67a2a0f5cb4304558b90662ae
karlos <49014526+karloskoronel@users.noreply.github.com>
2021-04-16 11:53:12 -0500

    haciendo que todas las paginas con la misma estructura (header y footer)

---
+++ assets/js/loader.js
@@ -0,0 +1,8 @@
+$('#section-header').load(
+    '../resources/blocks/navbar.html',
+    function (error) {}
+)
+$('#section-footer').load(
+    '../resources/blocks/footer.html',
+    function (error) {}
+)
