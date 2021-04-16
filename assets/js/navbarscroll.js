4844b4bdf31ca8f2ba117fb42d95fc5b35cdaeae
karlos <49014526+karloskoronel@users.noreply.github.com>
2021-04-16 09:52:48 -0500

    primer arreglo de menu - versiones incorrectas

--- assets/js/navbarscroll.js
+++ assets/js/navbarscroll.js
@@ -1,15 +1,15 @@
-let navbar = document.querySelector('#section-header .navbar')
-console.log(navbar)
+let sectionHeader = document.querySelector('#section-header')
+console.log(sectionHeader)
 let baseScroll = 0
 window.addEventListener('scroll', function (event) {
     last_known_scroll_position = window.scrollY
     if (
         last_known_scroll_position >= Math.floor(window.visualViewport.height)
     ) {
-        navbar.classList.remove('bg-transparent')
-        navbar.classList.add('bg-light')
+        sectionHeader.classList.remove('bg-transparent')
+        sectionHeader.classList.add('bg-light')
     } else {
-        navbar.classList.remove('bg-light')
-        navbar.classList.add('bg-transparent')
+        sectionHeader.classList.remove('bg-light')
+        sectionHeader.classList.add('bg-transparent')
     }
 })
