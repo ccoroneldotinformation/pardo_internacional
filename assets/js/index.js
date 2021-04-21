2229d7b6b78be6d94eeb3fc2a6aae5c224b0212c
karlos <49014526+karloskoronel@users.noreply.github.com>
2021-04-21 11:44:39 -0500

    cambios realizados: formulario distinguible al iniciar pagina, menu inicio, resaltar nombres de ciudades

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,7 @@ $('#section-header button').on('click', function () {
  */
 const upLineSearch = $('#section-search #search-core').parent()
 const upLineDescription = $('#section-description .row')
-const formSearch = $('#search-core')
+// const formSearch = $('#search-core')
 const imgSearch = $('#content-figure')
 const imgDescription = $('#section-description .content-image')
 const textDescription = $('#section-description .content-text-description')
@@ -33,8 +33,8 @@ $(window).on('scroll', function () {
         middleWindow < distanceDescription &&
         appliedEfectSearch == 0
     ) {
-        formSearch.addClass('animationIn-LeftToRight')
-        formSearch.removeClass('invisible')
+        // formSearch.addClass('animationIn-LeftToRight')
+        // formSearch.removeClass('invisible')
         imgSearch.addClass('animationIn-RightToLeft')
         imgSearch.removeClass('invisible')
         appliedEfectSearch = 1
